783c6541d952c62cd85578a8f3e2455d44c3cb12
Faraz Shariff <21FShariff@heckgrammar.co.uk>
2025-03-22 21:06:34 +0000

    finished hw

--- FixSubroutineTask/Program.cs
+++ FixSubroutineTask/Program.cs
@@ -4,14 +4,15 @@
     {
         static void Main(string[] args)
         {
-            for(int i = 8;i >= 0; i++) {
+            for(int i = 0;i < 8; i++) // makes the loop work properly
+            {
                 Console.WriteLine(diffCurrencies(i));            
             }
         }
         static string diffCurrencies(int x)
         {
             string[] currencies = { "baht", "dollar", "euro", "koruna", "lira", "rand", "rupee", "yen" };
-            return currencies[1];
+            return currencies[x]; //changes to int x instead of 1 
 
         }
     }
